d1176de35c4bdde09746bca262b7fca75a41d5cd
wndudrla1011 <wndudrla1011@naver.com>
2024-01-01 15:51:45 +0900

    edited Route's path

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router basename="{process.env.PUBLIC_URL}">
+    <Router>
       <Routes>
         <Route path="/hello" element={<h1>Hello</h1>} />
-        <Route path="/movie/:id" element={<Detail />} />
-        <Route path="/" element={<Home />} />
+        <Route
+          path={`${process.env.PUBLIC_URL}/movie/:id`}
+          element={<Detail />}
+        />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
       </Routes>
     </Router>
   );
