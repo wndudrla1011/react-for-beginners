b2596d1f6191aa0793d88df70c6f4a59aed2a343
wndudrla1011 <wndudrla1011@naver.com>
2024-01-01 15:42:48 +0900

    edited Route's path

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <Router basename="{process.env.PUBLIC_URL}">
       <Routes>
         <Route path="/hello" element={<h1>Hello</h1>} />
-        <Route path="movie/:id" element={<Detail />} />
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
