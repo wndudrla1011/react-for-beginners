12b8495cbc6b4f4ed5a4df65e536d2ed8e13d885
wndudrla1011 <wndudrla1011@naver.com>
2024-01-01 16:03:17 +0900

    switched BrowserRouter to Router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/hello" element={<h1>Hello</h1>} />
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
